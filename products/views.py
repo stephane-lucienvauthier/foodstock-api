2d3247dbe2f89668db0e196ad848e0b1867335de
Stephane Lucien-Vauthier <stephane.lucien-vauthier@pytech.fr>
2020-08-23 20:42:05 +0200

    Fix return code

--- products/views.py
+++ products/views.py
@@ -62,7 +62,7 @@ class ProductView(APIView):
         serializer = ProductUpdateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(owner=request.user)
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
